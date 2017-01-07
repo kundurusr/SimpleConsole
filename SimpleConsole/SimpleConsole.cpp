2ff3240687e7888ca01750f4c5b6b1dfbc96db13
Srinivasa Reddy Kunduru <kundurusr@gmail.com>
2017-01-07 22:35:29 +0800

    This is new repo for git

--- SimpleConsole/SimpleConsole.cpp
+++ SimpleConsole/SimpleConsole.cpp
@@ -1,11 +1,9 @@
-// SimpleConsole.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
-
+#include <stdio.h>
 
 int main()
 {
+
+	printf("Hello World!\n");
     return 0;
 }
 
